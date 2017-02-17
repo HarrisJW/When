92869043efe293ba77426f6db81d6106a3c661fb
Manticore <thelevimatusproject@gmail.com>
2017-02-17 18:31:05 -0400

    US1: LM/ZS: Added support for Google OAuth using Jetty (as opposed to the previous use of Sprint-Boot) in order to integrate correctly with the rest of the project.

--- src/main/java/com/vaadin/vaadin_archetype_application/MyUI.java
+++ src/main/java/com/vaadin/vaadin_archetype_application/MyUI.java
@@ -1,7 +1,13 @@
 package com.vaadin.vaadin_archetype_application;
 
-import com.vaadin.spring.annotation.SpringUI;
+import javax.servlet.annotation.WebServlet;
 
+import org.vaadin.addon.oauthpopup.OAuthListener;
+import org.vaadin.addon.oauthpopup.OAuthPopupButton;
+import org.vaadin.addon.oauthpopup.buttons.GoogleButton;
+
+import com.github.scribejava.core.model.Token;
+import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
@@ -19,8 +25,8 @@ import com.vaadin.ui.VerticalLayout;
  * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
  * overridden to add component to the user interface and initialize non-component functionality.
  */
+@Push
 @Theme("mytheme")
-@SpringUI(path = "/ui/logon")
 public class MyUI extends UI {
 
 	// init() is the new main()
@@ -35,6 +41,23 @@ public class MyUI extends UI {
 
         // Add a new button to the interface. 
         Button button = new Button("Click Me");
+        String GGL_KEY = "CLIENT KEY HERE";
+        String GGL_SECRET = "CLIENT SECRET HERE";
+        OAuthPopupButton ob = new GoogleButton(GGL_KEY, GGL_SECRET, "https://www.googleapis.com/auth/calendar");
+        ob.addOAuthListener(new OAuthListener() {
+        	@Override
+        	public void authSuccessful(Token token, boolean isOAuth20) {
+        		// TODO Auto-generated method stub
+        		System.out.println(token.toString());
+        	}
+
+        	@Override
+        	public void authDenied(String reason) {
+        		// TODO Auto-generated method stub
+        		System.out.println("DENIED");
+        	}
+        });
+
         
         // Event object e. Add new component to layout.
         // lambda function. Read up.
@@ -43,15 +66,16 @@ public class MyUI extends UI {
                     + ", it works!"));
         });
         
-        layout.addComponents(name, button);
+        layout.addComponents(name, button, ob);
         layout.setMargin(true);
         layout.setSpacing(true);
         
         setContent(layout);
     }
 
-//    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-//    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-//    public static class MyUIServlet extends VaadinServlet {
-//    }
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+
 }
