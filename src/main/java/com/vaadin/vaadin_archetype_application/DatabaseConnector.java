074e5a924c39983f1956ccb6f1104ce087e103ca
Levi Matus <thelevimatusproject@gmail.com>
2017-03-17 00:01:40 -0300

    Added Support for stored procs
    -Glib, Levi

--- src/main/java/com/vaadin/vaadin_archetype_application/DatabaseConnector.java
+++ src/main/java/com/vaadin/vaadin_archetype_application/DatabaseConnector.java
@@ -1,5 +1,7 @@
 package com.vaadin.vaadin_archetype_application;
 
+import static org.junit.Assert.assertEquals;
+
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -49,7 +51,7 @@ public class DatabaseConnector extends MySQLProvider {
 	//Function that checks if meeting ID and password correspond to any existing ones and 
 	//joins users to that meeting
 	@Override
-	public int TryJoinMeeting(String meetingID, String meetingPassword)//TODO
+	public int TryJoinMeeting(String meetingID, String meetingPassword, int userID)//TODO
 	{
 		if (meetingID.length() == 0)
 			return Constants.CODE_MEETING_ID_EMPTY;
@@ -65,57 +67,225 @@ public class DatabaseConnector extends MySQLProvider {
 	
 	
 	@Override
-	public int CreateMeeting(String password, Date startDate, Date endDate, String name, Date duration) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int CreateMeeting(String password, Date startDate, Date endDate, String name, Date duration, int userID) {
+		DBProvider dbc = new DatabaseConnector();
+		dbc.Initialize();
+		//ArrayList<Object[]> r = DatabaseConnector.ExecuteQuery(DatabaseConnector.OpenConnection(), "SELECT * FROM user;");
+		ArrayList<Object[]> r = dbc.ExecuteStoredProcedure(dbc.GetConnection(), "createMeeting", new Object[] { password, startDate,
+				endDate, name, duration, userID});
+		for (int i = 0; i < r.size(); i++)
+		{
+			Object[] o = r.get(i);
+			System.out.print((int)o[0] + ", ");
+			System.out.println("");
+		}
+		//assertEquals(2, dbc.GetUserID("2asd@asd.asd"));
+		dbc.Dispose();
+		Object[] o = r.get(0);
+		return (int)o[0];
 	}
 
 	@Override
 	public boolean DeleteMeeting(int meetingID) {
-		// TODO Auto-generated method stub
+		DBProvider dbc = new DatabaseConnector();
+		dbc.Initialize();
+		//ArrayList<Object[]> r = DatabaseConnector.ExecuteQuery(DatabaseConnector.OpenConnection(), "SELECT * FROM user;");
+		ArrayList<Object[]> r = dbc.ExecuteStoredProcedure(dbc.GetConnection(), "deleteMeeting", new Object[] { meetingID });
+		//assertEquals(2, dbc.GetUserID("2asd@asd.asd"));
+		dbc.Dispose();
 		return false;
 	}
 
 	@Override
 	public MeetingDescription GetMeetingDescription(int meetingID) {
-		// TODO Auto-generated method stub
-		return null;
+		DBProvider dbc = new DatabaseConnector();
+		dbc.Initialize();
+		//ArrayList<Object[]> r = DatabaseConnector.ExecuteQuery(DatabaseConnector.OpenConnection(), "SELECT * FROM user;");
+		ArrayList<Object[]> r = dbc.ExecuteStoredProcedure(dbc.GetConnection(), "getMeetingDescription", new Object[] { meetingID });
+		Object[] o = null;
+		for (int i = 0; i < r.size(); i++)
+		{
+			o = r.get(i);
+			System.out.print((String)o[0] + ", ");
+			System.out.print((Date)o[1] + ", ");
+			System.out.print((Date)o[2] + ", ");
+			System.out.print((Date)o[3] + ", ");
+			System.out.print((int)o[4] + ", ");
+			System.out.println("");
+		}
+		//assertEquals(2, dbc.GetUserID("2asd@asd.asd"));
+		dbc.Dispose();
+		MeetingDescription meetingDescription = new MeetingDescription();
+		if (o == null) {
+			meetingDescription.name = (String)o[0];
+			meetingDescription.startDate = (Date)o[1];
+			meetingDescription.endDate = (Date)o[2];
+			meetingDescription.duration = (Date)o[3];
+			meetingDescription.state = MeetingState.Setup;
+		} else {
+			meetingDescription.name = (String)o[0];
+			meetingDescription.startDate = (Date)o[1];
+			meetingDescription.endDate = (Date)o[2];
+			meetingDescription.duration = (Date)o[3];
+			if ((int)o[4]==0)
+				meetingDescription.state = MeetingState.Setup;
+			else if ((int)o[4]==1)
+				meetingDescription.state = MeetingState.Voting;
+			else
+				meetingDescription.state = MeetingState.Finalized;
+		}
+		return meetingDescription;
 	}
 
 	@Override
-	public MeetingShortDescription[] GetMeetingsList() {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<MeetingShortDescription> GetMeetingsList(int userID) {
+		DBProvider dbc = new DatabaseConnector();
+		dbc.Initialize();
+		//ArrayList<Object[]> r = DatabaseConnector.ExecuteQuery(DatabaseConnector.OpenConnection(), "SELECT * FROM user;");
+		ArrayList<Object[]> r = dbc.ExecuteStoredProcedure(dbc.GetConnection(), "getMeetingList", new Object[] { userID });
+		Object[] o;
+		ArrayList<MeetingShortDescription> meetingDescriptions = new ArrayList<MeetingShortDescription>();
+		for (int i = 0; i < r.size(); i++)
+		{
+			MeetingShortDescription msd = new MeetingShortDescription();
+			o = r.get(i);
+			System.out.print((int)o[0] + ", ");
+			msd.ID = (int)o[0];
+			System.out.print((String)o[1] + ", ");
+			msd.name = (String)o[1];
+			System.out.print((int)o[2] + ", ");
+			msd.ID = (int)o[2];
+			System.out.print((int)o[3] + ", ");
+			msd.ID = (int)o[3];
+			System.out.println("");
+			meetingDescriptions.add(msd);
+		}
+		//assertEquals(2, dbc.GetUserID("2asd@asd.asd"));
+		dbc.Dispose();
+		return meetingDescriptions;
 	}
 
 	@Override
-	public MeetingMember[] GetMeetingMembers(int meetingID) {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<MeetingMember> GetMeetingMembers(int meetingID) {
+		DBProvider dbc = new DatabaseConnector();
+		dbc.Initialize();
+		//ArrayList<Object[]> r = DatabaseConnector.ExecuteQuery(DatabaseConnector.OpenConnection(), "SELECT * FROM user;");
+		ArrayList<Object[]> r = dbc.ExecuteStoredProcedure(dbc.GetConnection(), "getMeetingMembers", new Object[] { meetingID });
+		Object[] o;
+		ArrayList<MeetingMember> meetingMembers = new ArrayList<MeetingMember>();
+		for (int i = 0; i < r.size(); i++)
+		{
+			MeetingMember mm = new MeetingMember();
+			o = r.get(i);
+			System.out.print((int)o[0] + ", ");
+			mm.ID = (int)o[0];
+			System.out.print((String)o[1] + ", ");
+			mm.firstName = (String)o[1];
+			System.out.print((String)o[2] + ", ");
+			mm.lastName = (String)o[2];
+			System.out.print((int)o[3] + ", ");
+			if ((int)o[3]==0)
+				mm.access = UserAccess.Member;
+			else if ((int)o[3]==1)
+				mm.access = UserAccess.Moderator;
+			else
+				mm.access = UserAccess.Creator;
+			System.out.println("");
+			meetingMembers.add(mm);
+		}
+		//assertEquals(2, dbc.GetUserID("2asd@asd.asd"));
+		dbc.Dispose();
+		return meetingMembers;
 	}
 
 	@Override
 	public int GetMeetingMembersCount(int meetingID) {
-		// TODO Auto-generated method stub
-		return 0;
+		DBProvider dbc = new DatabaseConnector();
+		dbc.Initialize();
+		//ArrayList<Object[]> r = DatabaseConnector.ExecuteQuery(DatabaseConnector.OpenConnection(), "SELECT * FROM user;");
+		ArrayList<Object[]> r = dbc.ExecuteStoredProcedure(dbc.GetConnection(), "getMeetingMembersCount", new Object[] { meetingID });
+		for (int i = 0; i < r.size(); i++)
+		{
+			Object[] o = r.get(i);
+			System.out.print((int)o[0] + ", ");
+			System.out.println("");
+		}
+		//assertEquals(2, dbc.GetUserID("2asd@asd.asd"));
+		dbc.Dispose();
+		Object[] o = r.get(0);
+		return (int)o[0];
 	}
 
 	@Override
 	public int GetUserID(String email) {
-		// TODO Auto-generated method stub
-		return 0;
+		DBProvider dbc = new DatabaseConnector();
+		dbc.Initialize();
+		//ArrayList<Object[]> r = DatabaseConnector.ExecuteQuery(DatabaseConnector.OpenConnection(), "SELECT * FROM user;");
+		ArrayList<Object[]> r = dbc.ExecuteStoredProcedure(dbc.GetConnection(), "getUserID", new Object[] { email });
+		for (int i = 0; i < r.size(); i++)
+		{
+			Object[] o = r.get(i);
+			System.out.print((int)o[0] + ", ");
+			System.out.println("");
+		}
+		//assertEquals(2, dbc.GetUserID("2asd@asd.asd"));
+		dbc.Dispose();
+		Object[] o = r.get(0);
+		return (int)o[0];
 	}
 
 	@Override
 	public boolean UpdateMeetingTime(int meetingID, Date startDate, Date endDate, Date duration) {
-		// TODO Auto-generated method stub
+		DBProvider dbc = new DatabaseConnector();
+		dbc.Initialize();
+		//ArrayList<Object[]> r = DatabaseConnector.ExecuteQuery(DatabaseConnector.OpenConnection(), "SELECT * FROM user;");
+		ArrayList<Object[]> r = dbc.ExecuteStoredProcedure(dbc.GetConnection(), "updateMeetingTime", new Object[] { meetingID, startDate,
+				endDate, duration });
+		for (int i = 0; i < r.size(); i++)
+		{
+			Object[] o = r.get(i);
+			System.out.print((int)o[0] + ", ");
+			System.out.println("");
+		}
+		//assertEquals(2, dbc.GetUserID("2asd@asd.asd"));
+		dbc.Dispose();
 		return false;
 	}
 
 	@Override
-	public boolean CreateUser(String firstName, String lastName, String email, String googleID) {
-		// TODO Auto-generated method stub
-		return false;
+	public int CreateUser(String firstName, String lastName, String email, String googleID) {
+		DBProvider dbc = new DatabaseConnector();
+		dbc.Initialize();
+		//ArrayList<Object[]> r = DatabaseConnector.ExecuteQuery(DatabaseConnector.OpenConnection(), "SELECT * FROM user;");
+		ArrayList<Object[]> r = dbc.ExecuteStoredProcedure(dbc.GetConnection(), "createUser", new Object[] { firstName, lastName, email, googleID });
+		for (int i = 0; i < r.size(); i++)
+		{
+			Object[] o = r.get(i);
+			System.out.print((int)o[0] + ", ");
+			System.out.println("");
+		}
+		//assertEquals(2, dbc.GetUserID("2asd@asd.asd"));
+		dbc.Dispose();
+		Object[] o = r.get(0);
+		return (int)o[0];
+	}
+	
+	public int LeaveMeeting(int meetingID, int userID) {
+		DBProvider dbc = new DatabaseConnector();
+		dbc.Initialize();
+		//ArrayList<Object[]> r = DatabaseConnector.ExecuteQuery(DatabaseConnector.OpenConnection(), "SELECT * FROM user;");
+		ArrayList<Object[]> r = dbc.ExecuteStoredProcedure(dbc.GetConnection(), "leaveMeeting", new Object[] { meetingID, userID });
+		for (int i = 0; i < r.size(); i++)
+		{
+			Object[] o = r.get(i);
+			System.out.print((int)o[0] + ", ");
+			System.out.println("");
+		}
+		//assertEquals(2, dbc.GetUserID("2asd@asd.asd"));
+		dbc.Dispose();
+		Object[] o = r.get(0);
+		return (int)o[0];
+		
 	}
 	
 	
