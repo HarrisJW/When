d2e01377134cde413c0571568911ea63e1e5359a
max parsons <Max.Parsons@dal.ca>
2017-03-29 08:43:21 -0300

    Create method for sharing google calendar between users - MP & JH

--- src/main/java/com/vaadin/vaadin_archetype_application/CalendarStuff.java
+++ src/main/java/com/vaadin/vaadin_archetype_application/CalendarStuff.java
@@ -17,29 +17,30 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.Calendar;
-import com.google.api.services.calendar.Calendar.CalendarList;
-import com.google.api.services.calendar.Calendar.Freebusy;
-import com.google.api.services.calendar.model.CalendarListEntry;
+import com.google.api.services.calendar.Calendar.Acl;
+import com.google.api.services.calendar.model.AclRule;
+import com.google.api.services.calendar.model.AclRule.Scope;
 import com.google.api.services.calendar.model.FreeBusyRequest;
 import com.google.api.services.calendar.model.FreeBusyRequestItem;
 import com.google.api.services.calendar.model.FreeBusyResponse;
 import com.vaadin.ui.UI;
 
 public class CalendarStuff {
+	
+	private static GoogleCredential credential = (GoogleCredential) UI.getCurrent().getSession().getAttribute("credential");
 	//test to calculate busy times for a single calendar
-		public static void calendarTest() throws IOException, ParseException
+		public static FreeBusyResponse calendarTest() throws IOException, ParseException
 		{
 			HttpTransport httpTransport = new NetHttpTransport();
 	        JsonFactory jsonFactory = new JacksonFactory();
-			GoogleCredential credential = (GoogleCredential) UI.getCurrent().getSession().getAttribute("credential");
 			
 			HttpRequestInitializer initializer = credential;
 			
 			Calendar calendar = new Calendar(httpTransport,jsonFactory,initializer);
 			
 			//specify start and end time for busy query
-			String dIn = "2017-03-19 00:00:00";
-			String dIne = "2017-03-25 23:99:9";
+			String dIn = "2017-03-27 00:00:00";
+			String dIne = "2017-03-28 23:99:99";
 			DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 			Date d = df.parse(dIn);
@@ -52,10 +53,14 @@ public class CalendarStuff {
 			req.setTimeMin(startTime);
 			req.setTimeMax(endTime);
 			req.setTimeZone("America/Halifax");
+			
 			FreeBusyRequestItem item = new FreeBusyRequestItem();
+			FreeBusyRequestItem item2 = new FreeBusyRequestItem();
+			item2.setId("maxaaronparsons@gmail.com");
 			item.setId("whenapp3130@gmail.com");//specify calendar id
 			List<FreeBusyRequestItem> list = new ArrayList<FreeBusyRequestItem>();
 			list.add(item);
+			list.add(item2);
 			req.setItems(list);
 			
 			//query the calendar
@@ -65,5 +70,25 @@ public class CalendarStuff {
 			//print response
 			System.out.println(fbresponse.toString());
 			
+			return fbresponse;
 		}
+		
+		public static void shareCalendar(String email) throws IOException 
+		{
+			HttpTransport httpTransport = new NetHttpTransport();
+	        JsonFactory jsonFactory = new JacksonFactory();
+	        
+			Calendar calendar = new Calendar(httpTransport,jsonFactory,credential);
+			
+			AclRule rule = new AclRule();
+			Scope scope = new Scope();
+			scope.setType("user");
+			scope.setValue("email");
+			
+			rule.setScope(scope);
+			rule.setRole("freeBusyReader");
+			AclRule inserted = calendar.acl().insert("primary", rule).execute();
+		}
+		
+		
 }
